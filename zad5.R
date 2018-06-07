ff57f1743dae306ae5bb05dc9abb0dabc70e8764
mszewc <38323724+mszewc@users.noreply.github.com>
2018-06-07 10:23:52 +0200

    usunęłam i dodałam spacje
    Czy teraz już jest ładnie?

--- zad5.R
+++ zad5.R
@@ -6,7 +6,7 @@ movies
 
 # zad5-2
 
-filter(movies, year == 2005, Comedy == 1 )
+filter(movies, year == 2005, Comedy == 1)
 
 # zad5-3
 
@@ -16,12 +16,12 @@ filter(movies, year == 2005, Comedy == 1 )
       
 
 # zad5-4
-# Uzna³am, ¿e rating > 7 oznacza dobry film.
+# Uznałam, że rating > 7 oznacza dobry film.
 
       movies %>%
      filter(Animation == 1, year >= 1990 & year < 2000, votes >= (mean(votes)) , rating > 7) %>%
         arrange(desc(rating)) %>% 
-        select(title,year,rating)
+        select(title, year, rating)
  
 
 # zad5-5
@@ -39,7 +39,7 @@ filter(movies, year == 2005, Comedy == 1 )
      select(rating, mpaa) %>%
         filter(mpaa != "NA") %>%
   group_by(mpaa) %>%
-    summarise( rating_mean = mean(rating, na.rm = T), rating_mad = mad(rating, na.rm = T))
+    summarise(rating_mean = mean(rating, na.rm = T), rating_mad = mad(rating, na.rm = T))
   
   
 
